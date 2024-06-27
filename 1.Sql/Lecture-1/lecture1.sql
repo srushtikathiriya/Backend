fec79ffe45d82fe046567e0a1651ffe15b778416
srushti <srushtikathiriya2003@gmail.com>
2024-06-27 11:00:10 +0530

    27-06-2024

---
+++ 1.Sql/Lecture-1/lecture1.sql
@@ -0,0 +1,30 @@
+➡️Connect mysql
+    ---> \sql
+    --->  \connect root@localhost:3306
+    --->   password:Admin@123
+
+➡️show all databases command:
+    ---> show databases;
+
+➡️create database command:
+    ---> create database <databasename>;
+    example:create database students;
+
+➡️use particular database command:
+    ---> use <databasename>;
+    example:use students;
+
+➡️delete database command:
+    ---> drop database <databasename>;
+    example:drop database students;    
+
+➡️show particular database tables;
+    ---> show tables;
+
+➡️create new table command:
+       ---> create table user(firstname varchar(30),lastname varchar(30),email varchar(50),gender varcar(10),mobileNo int);
+
+➡️describe or show perticular table(row and column)
+        --->desc user;
+                or
+        --->describe user;        
